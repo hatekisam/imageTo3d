da4eddc3b01b2673df15f261d03d4b5a3423c4f1
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 18:39:48 +0200

    finished the drag and drop of image

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,23 +1,55 @@
 "use client";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 const Uploader = () => {
+  const [selectedImage, setSelectedImage] = useState<any>(null);
+
   const onDrop = useCallback((acceptedFiles: any) => {
-    console.log(acceptedFiles);
+    const file = acceptedFiles[0];
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please select a valid image file.");
+    }
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div className="w-full flex items-center justify-center flex-col gap-3">
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
+    <div className="w-full h-screen flex items-center justify-center flex-col gap-3">
+      <div
+        {...getRootProps()}
+        className="w-[400px] h-[400px] rounded-md border-2 border-blue-400 p-5 flex items-center justify-center"
+      >
+        <input {...getInputProps()} accept="image/*" />
+        {selectedImage ? (
+          <img
+            src={selectedImage}
+            alt="Selected"
+            className="w-[300px]"
+          />
         ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <>
+            {isDragActive ? (
+              <p className="text-gray-500 text-center text-lg">
+                Drop the files here ...
+              </p>
+            ) : (
+              <p className="text-gray-500 text-center text-lg">
+                Drag 'n' drop an image here, or click to select an image
+              </p>
+            )}
+          </>
         )}
       </div>
-      <button>Upload Image</button>
+      <button className="bg-blue-500 px-4 py-2 rounded-lg text-white">
+        Get 3D Model
+      </button>
     </div>
   );
 };
