71e71dfe0c8d794e6c6b933e81d12cea27f51219
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-14 18:30:49 +0200

    Added the react dropzone

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,8 +1,25 @@
 "use client";
-import React from "react";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 const Uploader = () => {
-  return <div>Uploader</div>;
+  const onDrop = useCallback((acceptedFiles: any) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div className="w-full flex items-center justify-center flex-col gap-3">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      <button>Upload Image</button>
+    </div>
+  );
 };
 
 export default Uploader;
